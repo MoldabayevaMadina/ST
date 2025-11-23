9751dce84d78e6a4b6580fdea45b362c83dc5908
MoldabayevaMadina <98735146+MoldabayevaMadina@users.noreply.github.com>
2025-11-23 23:11:51 +0500

    changes in browsers.py

--- ST_Assignments3_4/browsers.py
+++ ST_Assignments3_4/browsers.py
@@ -1,3 +1,4 @@
+import os
 def get_caps(browser, username, access_key, test_name):
     caps = {
         "browserName": browser,
@@ -8,7 +9,7 @@ def get_caps(browser, username, access_key, test_name):
             "userName": username,
             "accessKey": access_key,
             "projectName": "Homework 3+4",
-            "buildName": "Build 1",
+            "buildName": f"Jenkins-{os.getenv('JOB_NAME')}-{os.getenv('BUILD_NUMBER')}",
             "sessionName": test_name
         }
     }
